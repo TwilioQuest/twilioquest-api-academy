6e90513c681be8a4ace6e84313705e849597e416
Ryan Kubik <rkubik@twilio.com>
2021-10-19 19:51:28 -0700

    Fix REST acronym to URL in neumann chest

--- levels/api_academy/objectives/neumann_initiation/validator.js
+++ levels/api_academy/objectives/neumann_initiation/validator.js
@@ -1,21 +1,21 @@
 module.exports = async (helper) => {
-  const answer1 = helper.getNormalizedInput('answer1');
-  const answer2 = helper.getNormalizedInput('answer2');
+  const answer1 = helper.getNormalizedInput("answer1");
+  const answer2 = helper.getNormalizedInput("answer2");
 
-  if (answer1 === '' || answer2 === '') {
+  if (answer1 === "" || answer2 === "") {
     return helper.fail(`
       Please answer both questions!
     `);
   }
 
-  if (answer1 !== 'resource') {
+  if (answer1 !== "resource") {
     return helper.fail(`
       The first answer is incorrect - the name for an object that you can
-      access via HTTP in a RESTful API is the "R" in "REST".
+      access via HTTP in a RESTful API is the "R" in "URL".
     `);
   }
 
-  if (answer2 !== 'post') {
+  if (answer2 !== "post") {
     return helper.fail(`
       The second answer is incorrect - Craig talks about mapping "CRUD" 
       operations (Create, Read, Update, and Delete) in his video - check the
