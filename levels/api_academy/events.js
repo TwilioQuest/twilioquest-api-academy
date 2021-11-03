f6ab318ed6bd73427f20396f06824e0b745ec130
Ryan Kubik <rkubik@twilio.com>
2021-11-03 12:01:51 -0700

    Update quest log on complete

--- levels/api_academy/events.js
+++ levels/api_academy/events.js
@@ -1,5 +1,7 @@
 const merge = require("lodash.merge");
 const processInitiationEvents = require("./events/initiation");
+const packageInfo = require("../../package.json");
+const updateQuestLogWhenComplete = require("./events/updateQuestLogWhenComplete");
 
 const INITIAL_STATE = {
   initiation: {
@@ -14,6 +16,16 @@ module.exports = async function (event, world) {
   const worldState = merge(INITIAL_STATE, world.getState(WORLD_STATE_KEY));
 
   processInitiationEvents(event, world, worldState);
+
+  updateQuestLogWhenComplete({
+    notification:
+      'I\'ve completed everything in the <span class="highlight">API Academy</span> for now!',
+    log: "I've completed everything in the API Academy for now!",
+    event,
+    world,
+    worldStateKey: WORLD_STATE_KEY,
+    version: packageInfo.version,
+  });
 
   world.setState(WORLD_STATE_KEY, worldState);
 };
