b8c7c1a4179496215697cbccd0411eb99df4ed14
rodrigostackbuilders <raparicio@stackbuilders.com>
2022-02-09 03:08:50 -0300

    Translations additional langs

---
+++ i18n/ja.js
@@ -0,0 +1,79 @@
+module.exports = {
+  "api-academy.professor-default.personal_intro_response": "あなたが誰なのか、ここで何をしているのかをもう一度教えてくれませんか?",
+  "api-academy.professor-default.exposition_response": "アカデミーの目的は何ですか?",
+  "api-academy.professor-default.get_started_response": "アカデミーに入るにはどうすればいいですか?",
+  "api-academy.professor-default.get_started_ack": "分かりました。すべての宝箱を見つけたらまた戻ってきます。",
+  "api-academy.professor-default.default_personal_intro": "アカデミーに入学したいのですが、その前にあなたのことを教えていただけませんか?",
+  "api-academy.professor-default.default": `オペレーターではありませんか。アカデミーへの入学の申し込みに来られたということで
+  よろしいですか?<br/><br/>
+  そうだといいのですが。今までArcane Academy of API Artsに
+  学びに来られたオペレーターは多くありませんでしたが、
+  みなさんが模範的な学生でした。`,
+  "api-academy.professor-default.follow_up_greeting": "オペレーター、また会いましたね。これからアカデミーで学びを深める準備は万端ですか?",
+  "api-academy.professor-default.thanks": `よくできました!あなたはアカデミーに入学するために必要な課題を達成しました。
+  残念ながら、アカデミーではまだ編入生を
+  受け入れる体制が整っていません。
+  <a href='https://www.twilio.com/quest#email'>こちらで登録</a>
+  を行えば、学習を開始できるようになったときに正式な入学許可の通知をお送りします。
+  <br/></br/>
+  <i>[あなたはTwilioQuestの開発者がアカデミーを完成させるまでにまだ時間を
+  必要としていると考え、後日戻ると決めます。]</i>`,
+  "api-academy.professor-default.get_started_allComplete": `あなたはアカデミーへの入学に必要な資質があることを証明しました。
+  あとは編入生がアカデミーへのアクセスを許可されるまで待つだけです。
+  <a href='https://www.twilio.com/quest#email'>こちらで登録</a>
+  を行えば、アカデミーへの入学が可能になったときに正式な入学許可の通知をお送りします。`,
+  "api-academy.professor-default.get_started": `入学希望者はアカデミーへの入学前にAPIの基本的な使用方法を
+  マスターしている必要があります。南にある森に
+  <span class='highlight'>宝箱が4つ</span>あります。
+  それぞれを開いて私の後ろにある火鉢に火をつけてください。<br/><br/>
+  4つの火鉢に火をつけることで、あなたがArcane Academy of API Artsへの
+  入学に必要な資質を持っていると証明したことになります。`,
+  "api-academy.professor-default.ack": `探索がうまくいくようお祈りします。宝箱はそれぞれアカデミーの4つのスチューデントハウス
+  (Lovelace、Turing、von Neumann、Hopper)の
+  小さなモニュメントの中にあります。各ハウスの
+  <span class='highlight'>色を持つ石の構造物</span>を探して
+  ください。`,
+  "api-academy.professor-default.exposition": `APIはプログラミングの基本的な構成要素です。作成するコードの
+  ほとんどは他の誰かが作成したコードを呼び出すものであるため、
+  こうした概念をマスターすることは極めて重要です。<br/><br/>
+  また、HTTPを使用してインターネット上でAPIを使用できるようになれば、
+  コードで可能なことが大きく広がります。この可能性について学生に教えることがアカデミーの
+  第一の目的です。`,
+  "api-academy.professor-default.personal_intro": `私は<span class='highlight'>Kimberly Heapsort教授</span>、
+  Arcane Academy of API Artsの入学事務局の責任者です。学生が
+  当校の授業で学ぶ準備ができていることと、アカデミーでの教育が
+  あらゆる経歴を持った意欲的な開発者に開かれていることを確認しています。`,
+  "api-academy.initiation.completedHousesNotification": `{house} <i>{completedHouses}</i>の儀式を達成できた。残り{completedHousesLength}だ。`,
+  "api-academy.initiation.completedHousesNotification4": "すべてのハウスの儀式を達成できた。アカデミーの入口に戻ってみよう。",
+  "api-academy.events.notification": "<span class='highlight'>APIアカデミー</span>でできることはすべて完了できた!",
+  "api-academy.events.log": "APIアカデミーでできることはすべて完了できた!",
+  "api-academy.hopper_initiation.both": "両方の質問にお答えください。",
+  "api-academy.hopper_initiation.firstIncorrect": `最初の答えが正しくありません。リモートAPIは、ネットワーク上の他の
+  コンピューター(サーバー)にある処理リソースやコードを利用します。`,
+  "api-academy.hopper_initiation.secondIncorrect": `2番目の答えが正しくありません。質問した略語の正式名称は「Representational State Transfer」
+  です。Craigの動画で
+  4文字の略語をもう一度確認してください。`,
+  "api-academy.hopper_initiation.success": "正解です。House Hopperの知識の宝箱が開きました。",
+  "api-academy.lovelace_initiation.both": "両方の質問にお答えください。",
+  "api-academy.lovelace_initiation.firstIncorrect": `最初の答えが正しくありません。このプロトコルの略語は、
+  WebサイトのURLをブラウザに入力するときに最初に入力する4文字です。`,
+  "api-academy.lovelace_initiation.secondIncorrect": `2番目の答えが正しくありません。Webリクエストに伴う追加情報の名前は
+  「フッター」の対義語です。`,
+  "api-academy.lovelace_initiation.success": "正解です。House Lovelaceの知識の宝箱が開きました。",
+  "api-academy.neumann_initiation.both": "両方の質問にお答えください。",
+  "api-academy.neumann_initiation.firstIncorrect": `最初の答えが正しくありません。HTTPを使用してアクセスできる
+  RESTful API内のオブジェクトの名前は「URL」の「R」です。`,
+  "api-academy.neumann_initiation.secondIncorrect": `2番目の答えが正しくありません。Craigは動画の中で「CRUD」操作
+  (作成、読み取り、更新、削除)のマッピングについて話しています。
+  動画をもう一度見て、作成に対応するHTTPメソッドを確認してください。`,
+  "api-academy.neumann_initiation.success": "正解です。House Lovelaceの知識の宝箱が開きました。",
+  "api-academy.turing_initiation.both": "両方の質問にお答えください。",
+  "api-academy.turing_initiation.firstIncorrect": "最初の答えは正しくありません。",
+  "api-academy.turing_initiation.secondIncorrect": `2番目の答えが正しくありません。JavaScriptで関数を呼び出すには、
+  オブジェクトを参照する変数の後に
+  「.」演算子を使用し、開き括弧と閉じ括弧「()」を使用します。
+  オブジェクトはこうした関数を通じた
+  APIとなります。例で使用している、文字列をすべて大文字に変換する
+  API関数の名前は何ですか?`,
+  "api-academy.turing_initiation.success": "正解です。House Turingの知識の宝箱が開きました。"
+}
