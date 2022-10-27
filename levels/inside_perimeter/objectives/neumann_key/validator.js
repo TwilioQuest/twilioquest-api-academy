10615d238d5a15c45b11f4a445944aae84833d68
Marvel Johnson <marvjohnson@twilio.com>
2022-10-27 15:37:16 -0700

    Updated "HTTP Methods" Objective
    -Added check for if the player gave an invalid input for the first question
    -Added check for if the player gave the label of one of the options for the first question, instead of the letter assigned to it (e.g. "GET" instead of "a")
    -Added ability for player to give "update" or "c" as an answer for the first question
    -Fixed the first question's failure text, such that the statement "___ *is* an HTTP Method." is always true

--- levels/inside_perimeter/objectives/neumann_key/validator.js
+++ levels/inside_perimeter/objectives/neumann_key/validator.js
@@ -1,8 +1,20 @@
-module.exports = async (helper) => {
-  const answer1 = helper.getNormalizedInput('answer1');
-  const answer2 = helper.getNormalizedInput('answer2');
-  const answer3 = helper.getNormalizedInput('answer3');
+function isValidAnswer1Option(answer1, answer1OptionsMap) {
+  return Object.keys(answer1OptionsMap).some(
+    (option) =>
+      option === answer1 || answer1OptionsMap[option].toLowerCase() === answer1
+  );
+}
 
+module.exports = async (helper) => {
+  const answer1 = helper.getNormalizedInput("answer1");
+  const answer2 = helper.getNormalizedInput("answer2");
+  const answer3 = helper.getNormalizedInput("answer3");
+  const answer1OptionsMap = {
+    a: "GET",
+    b: "POST",
+    c: "UPDATE",
+    d: "DELETE",
+  };
 
   if (!answer1 || !answer2 || !answer3) {
     return helper.fail(`
@@ -10,25 +22,40 @@ module.exports = async (helper) => {
     `);
   }
 
-  if (answer1 !== 'c') {
+  if (!isValidAnswer1Option(answer1, answer1OptionsMap)) {
+    return helper.fail(
+      `"${answer1}" is not a valid answer! Please choose between "a", "b", "c", and "d"!`
+    );
+  }
+
+  if (answer1 !== "c" && answer1 !== "update") {
+    // Because of the "isValidAnswer1Option" check, we know that the player either entered a single letter answer
+    // or one of the labels (i.e. GET, POST, UPDATE, DELETE). If the player typed in a letter, it'll get the correct
+    // verb mapping, otherwise it'll uppercase the input.
     return helper.fail(`
-      Uh oh, the first challenge question is incorrect, ${answer1.toUpperCase()} *is* an HTTP Method. Visit the [MDN documentation to learn more about HTTP methods](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods) or check out the information provided here in the hack interface.
+      Uh oh, the first challenge question is incorrect, "${
+        answer1OptionsMap[answer1] || answer1.toUpperCase()
+      }" *is* an HTTP Method. Visit the [MDN documentation to learn more about HTTP methods](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods) or check out the information provided here in the hack interface.
     `);
   }
 
-  if (answer2 !== 'get') {
+  if (answer2 !== "get") {
     let failMessage;
 
-    if (answer2 === 'post') failMessage = 'POST is used to submit new information to the server sent in the request payload.'
-    if (answer2 === 'put') failMessage = 'PUT is used to replace a target resource on the server with new data sent in the request.'
-    if (answer2 === 'read') failMessage = 'READ isn\'t a valid HTTP method.'
+    if (answer2 === "post")
+      failMessage =
+        "POST is used to submit new information to the server sent in the request payload.";
+    if (answer2 === "put")
+      failMessage =
+        "PUT is used to replace a target resource on the server with new data sent in the request.";
+    if (answer2 === "read") failMessage = "READ isn't a valid HTTP method.";
 
     return helper.fail(`
       The second challenge question is not quite right! ${failMessage} Visit the <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods">MDN documentation to learn more about HTTP methods</a> or check out the information provided here in the hack interface.
     `);
   }
 
-  if (answer3 !== 'post') {
+  if (answer3 !== "post") {
     return helper.fail(`
       The third challenge question is incorrect, but you can check the <a href="https://www.twilio.com/docs/sms/api/message-resource#create-a-message-resource">Twilio Programmable SMS documentation on creating a message</a> to find the answer!
   `);
