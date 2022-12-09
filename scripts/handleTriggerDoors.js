82e054d73a3361de89653367536bc4d5eaad2691
Marvel Johnson <marvjohnson@twilio.com>
2022-12-09 10:43:18 -0800

    Updated handleTriggerDoors
    -Removed hack for smooth transitions between door opening/closing

--- scripts/handleTriggerDoors.js
+++ scripts/handleTriggerDoors.js
@@ -17,47 +17,11 @@ function runDoorAction(world, assignedDoors, action) {
       return assignedDoors.some((doorKey) => instance.key === doorKey);
     },
     (door) => {
-      const requiredCurrentState = action === "open" ? "closed" : "open";
-
-      // This handles players moving in and out of the door trigger by enqueuing a pending state
-      // if the trigger is entered/exited while the door is changing states. For example,
-      // if the door is opening and the player leaves the trigger, the "close" action will
-      // be queued up to run once the door is finished opening.
-      if (door.state.fsm.currentState !== requiredCurrentState) {
-        if (waitingForRequiredState) {
-          door.sprite.animations.currentAnim.onComplete.remove(
-            runPendingDoorAction
-          );
-        }
-
-        waitingForRequiredState = true;
-        door.sprite.animations.currentAnim.onComplete.addOnce(
-          runPendingDoorAction,
-          undefined,
-          undefined,
-          door,
-          action
-        );
-        return;
-      }
-
+      console.log(`Opening door: ${door}`);
       door.state.fsm.action(action);
     }
   );
 }
-
-function runPendingDoorAction(_, __, door, action) {
-  // Exploiting the message queue (i.e. using setTimeout) to ensure the door's transition from
-  // 'opening' or 'closing' to 'open' and 'closed', respectively, can happen before executing
-  // the pending action. This prevents the door from skipping the animation and instantly changing from 'open' to 'closed'
-  // and vice versa
-  setTimeout(() => {
-    waitingForRequiredState = false;
-    door.state.fsm.action(action);
-  });
-}
-
-let waitingForRequiredState = false;
 
 module.exports = function handleDoorControls(event, world, worldState) {
   if (
