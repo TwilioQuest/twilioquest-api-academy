40f04bcb2a4b68f41848b94b6c105dfa0868e0bf
Marvel Johnson <marvjohnson@twilio.com>
2022-12-09 12:38:13 -0800

    Various Updated And Bug Fixes
    -Updated position of player_entry_point in lovelacehallway to be closer to the library door
    -Removed unneeded “requiresCompletedLevels” property from doors and triggers
    -Removed collision tile that was blocking entrance to the library door in lovelacehallway
    -Removed code that checked for the completion of entire levels, in place of checking for specific objectives to have been finished
    -Removed door pairing code in lovelace_tower event.js, since it wasn't needed
    -Moved levelChange objects to be closer to the middle of their respective doors, so that the player has to walk through them and the open/close animations have enough time to play
    -Fixed bug where lovelace_tower/library books wouldn’t disappear after the player completed api-05-get-patch objective

--- levels/lovelace_tower/events.js
+++ levels/lovelace_tower/events.js
@@ -7,7 +7,6 @@ const INITIAL_STATE = {
     destroyedEntities: [],
     unlockedTransitions: [],
     hiddenEntities: [],
-    openedDoors: [],
     entities: {
       "api-door-1": {
         spell: {
@@ -18,11 +17,12 @@ const INITIAL_STATE = {
               },
             },
             successActions: {
-              enableLevelChange({ world, worldState }) {
+              enableLevelChange({ event, world, worldState }) {
                 world.enableTransitionAreas(
                   ({ instance }) => instance.key === "exit_to_lovelace_library"
                 );
-                worldState.insideLovelaceTower.openedDoors.push("api-door-1");
+                console.log("working");
+                event.target.state.fsm.action("open");
               },
             },
           },
@@ -74,8 +74,6 @@ module.exports = async function (event, world) {
     ["api-03-fetch", "api-hidden-door-3"],
   ];
 
-  const doorPairs = [["api-04-remote-and-local", "api-door-1"]];
-
   if (
     event.name === "objectiveCompleted" ||
     event.name === "objectiveCompletedAgain"
@@ -86,15 +84,6 @@ module.exports = async function (event, world) {
         !worldState.insideLovelaceTower.hiddenEntities.includes(hiddenDoorKey)
       ) {
         worldState.insideLovelaceTower.hiddenEntities.push(hiddenDoorKey);
-      }
-    });
-
-    doorPairs.forEach(([objectiveKey, doorKey]) => {
-      if (
-        event.objective === objectiveKey &&
-        !worldState.insideLovelaceTower.openedDoors.includes(doorKey)
-      ) {
-        worldState.insideLovelaceTower.openedDoors.push(doorKey);
       }
     });
   }
@@ -108,10 +97,7 @@ module.exports = async function (event, world) {
   });
 
   // Open doors
-  [
-    ...worldState.insideLovelaceTower.openedDoors,
-    ...worldState.insideLibrary.openedDoors,
-  ].forEach((openedDoorKey) => {
+  worldState.insideLibrary.openedDoors.forEach((openedDoorKey) => {
     world.forEachEntities(
       openedDoorKey,
       (door) => door.state && door.state.fsm && door.state.fsm.action("open")
@@ -187,39 +173,32 @@ module.exports = async function (event, world) {
         );
       }
     }
-  };
+  }
 
-  // Set obj4Complete 
-  if (world.isObjectiveCompleted("api-04-remote-and-local")){
+  // Set obj4Complete
+  if (world.isObjectiveCompleted("api-04-remote-and-local")) {
     worldState.obj4Complete = true;
-    world.forEachEntities("lovelace-tower-door", towerDoor => {
-      towerDoor.interactable = false;
-    });
-  };
+  }
 
   // Once the final objective has been hacked and closed, hide books and empty shelves
   if (world.isObjectiveCompleted("api-05-get-patch")) {
-    world.hideEntities(`bad-book`);
-    if (
-      event.name === "objectiveDidClose" &&
-      event.target.objectiveName === "api-05-get-patch"
-    ) {
-      world.hideEntities(`bad-book`);
-    };
+    world.hideEntities(`badBook`);
 
     world.enableTransitionAreas("exit_to_library_corridor");
 
     // As player tries to leave, trigger Fredric conversation
-    if (world.isObjectiveCompleted("api-05-get-patch")){
+    if (world.isObjectiveCompleted("api-05-get-patch")) {
       if (
         event.name === "triggerAreaWasEntered" &&
         event.target.key === "fredricTrigger" &&
         worldState.fredricNoteTriggered === false
       ) {
-        world.startConversation("fredric-threat-lovelace", "fredricNeutral.png");
+        world.startConversation(
+          "fredric-threat-lovelace",
+          "fredricNeutral.png"
+        );
       }
     }
-   
 
     // When Fredric trigger closes, Fredric letter becomes interactable
     if (
@@ -230,8 +209,8 @@ module.exports = async function (event, world) {
         note.interactable = true;
       });
       worldState.fredricNoteTriggered = true;
-    };
-  };
+    }
+  }
 
   world.setState(LOVELACE_TOWER_STATE_KEY, worldState);
 };
